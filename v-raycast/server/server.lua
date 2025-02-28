5b81d6d108cb84173379741a0ee0fddc7c5b85ce
SpMex <113365369+yungmexx@users.noreply.github.com>
2025-02-28 00:18:15 -0800

    Update server.lua

--- v-raycast/server/server.lua
+++ v-raycast/server/server.lua
@@ -1,5 +1,25 @@
-local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Commands.Add(Config.Command, "Enable raycast coords (God Only)", {}, false, function(source, args)
-    TriggerClientEvent('v-raycast:client:toggle', source)
-end, Config.Permissions)
+RegisterCommand(Config.Command, function(source, args, rawCommand)
+    local playerId = GetPlayerIdentifier(source, 0)
+
+    if Config.UsePermissions then
+        if playerId and IsPlayerAdmin(playerId) then
+            TriggerClientEvent('v-raycast:client:toggle', source)
+        else
+            TriggerClientEvent('chat:addMessage', source, {
+                args = { '^1ERROR', 'You do not have permission to use this command.' }
+            })
+        end
+    else
+        TriggerClientEvent('v-raycast:client:toggle', source)
+    end
+end, false)
+
+function IsPlayerAdmin(playerId)
+    for _, admin in ipairs(Config.Permission) do
+        if playerId == admin then
+            return true
+        end
+    end
+    return false
+end
