42fca98a3b0094e6f9d2c8f5aeeeb610984d0f68
SpMex <113365369+yungmexx@users.noreply.github.com>
2025-02-28 00:17:53 -0800

    Update config.lua

--- v-raycast/config.lua
+++ v-raycast/config.lua
@@ -1,10 +1,16 @@
 Config = {}
 
 Config.Command = 'raycast' -- Command used to start raycast
-Config.Permission = 'god' -- permssions to use command -- options: 'god' 'admin' or false
 
+ -- Permssion to use command
+Config.UsePermissions = true
+Config.Permission = {
+    "steam:123456789012345",  -- Replace with actual Steam IDs or other identifiers of admins
+    "license:abcdef123456",   -- Another identifier example
+    -- Add more
+}
 
-Config.BoxColor = {r = 255, g = 255, b = 255, a = 255}
+Config.BoxColor = {r = 255, g = 255, b = 255, a = 255} -- Color of the box
 
-Config.LineColor =  {r = 255, g = 255, b = 255, a = 200}
-Config.HighlightedLineColor = {r = 0, g = 191, b = 255, a = 200}
+Config.LineColor =  {r = 255, g = 255, b = 255, a = 200} -- Color of the line
+Config.HighlightedLineColor = {r = 0, g = 191, b = 255, a = 200} -- Color of the line when raycasted on an entity
